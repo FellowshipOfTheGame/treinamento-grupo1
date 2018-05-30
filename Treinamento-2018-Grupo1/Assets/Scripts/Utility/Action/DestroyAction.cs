8ba5e2fc92cdd4c679ceb96666911b805972e614
Unknown <luis.eduardo.rozante@gmail.com>
2018-05-30 20:29:18 -0300

    Modifications to scripts and placeholder art.
    Modified some scripts and added some placeholder art to the game.

---
+++ Treinamento-2018-Grupo1/Assets/Scripts/Utility/Action/DestroyAction.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+// Destrói os objetos alvo.
+[AddComponentMenu("Scripts/Utility/Actions/Destroy")]
+public class DestroyAction : ActionBase {
+
+    // Lista de objetos a serem destruídos.
+    public List<Object> targets;
+
+    public override void Activate() {
+
+        base.Activate();
+
+        // Passa pela lista e destrói os objetos.
+        for (int i = 0; i < targets.Count; i++)
+            Destroy(targets[i]);
+
+    }
+}
---
+++ Treinamento-2018-Grupo1/Assets/Scripts/Utility/HabilitySource.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+// Esse script serve como fonte de habilidades para o player.
+[AddComponentMenu("Scripts/Utility/Hability Source")]
+public class HabilitySource : MonoBehaviour {
+
+    // Nome da habilidade a ser dada ao player. Deve estar contida entre as habilidades adicionadas na lista de habilidades
+    // no script do player.
+    public string habilityName = "newHability";
+
+    // Guarda se o player está na área em que pode ativar esse script.
+    private bool isOnRange = false;
+
+    // Verifica quando o player entra na área.
+    void OnTriggerEnter2D(Collider2D collision) {
+
+        if (collision.gameObject.tag == "Player")
+            isOnRange = true;
+
+    }
+
+    // Verifica quando o player sai da área.
+    void OnTriggerExit2D(Collider2D collision) {
+
+        if (collision.gameObject.tag == "Player")
+            isOnRange = false;
+
+    }
+
+    void Update() {
+
+        // Verifica se o player está dentro da área de ativação.
+        if(isOnRange)
+            if (Player.player.input.useButton) // Detecta quando o player tenta usar este objeto.
+                Player.player.SetHability(habilityName);
+
+    }
+}
