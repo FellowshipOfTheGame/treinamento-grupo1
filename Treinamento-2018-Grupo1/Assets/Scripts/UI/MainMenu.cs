c037bce23cf1c7026b48dfb6437044e07c72ec12
Unknown <luis.eduardo.rozante@gmail.com>
2018-07-30 12:56:20 -0300

    Made options menu work
    - Options menu now works! With the exception of the music slider, but we don't have any music (in fact we don't have any sound...) yet, so...
    - Removed graphic quality options as they don't do much in 2D.

--- Treinamento-2018-Grupo1/Assets/Scripts/UI/MainMenu.cs
+++ Treinamento-2018-Grupo1/Assets/Scripts/UI/MainMenu.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System.Collections.Generic;
 
 // Script do menu principal.
 [AddComponentMenu("Scripts/UI/Main Menu")]
@@ -14,6 +15,7 @@ public class MainMenu : MonoBehaviour {
     [Header("- Resolução:")]
     // Dropdown das resoluções.
     public Dropdown resolutionDropdown;
+    private Resolution[] avaliableResolutions;
     // Toggle de tela cheia.
     public Toggle fullscreenToogle;
 
@@ -22,6 +24,45 @@ public class MainMenu : MonoBehaviour {
     public Slider masterVolumeSlider;
     // Slider do volume da música
     public Slider musicVolumeSlider;
+
+    public void Start() {
+
+        // Cria uma lista de resoluções disponíveis e adiciona elas nas opções do Dropdown, também detecta qual é a opção de resolução atual.
+        avaliableResolutions = Screen.resolutions;
+        List<Dropdown.OptionData> resOptions = new List<Dropdown.OptionData>();
+        int currentResIndex = 0;
+
+        for(int i = 0; i < avaliableResolutions.Length; i++) {
+
+            Dropdown.OptionData opt = new Dropdown.OptionData();
+            opt.text = avaliableResolutions[i].width + "x" + avaliableResolutions[i].height + " (" + avaliableResolutions[i].refreshRate + "Hz)";
+            resOptions.Add(opt);
+
+            if (avaliableResolutions[i].width == Screen.width && avaliableResolutions[i].height == Screen.height)
+                currentResIndex = i;
+
+        }
+
+        resolutionDropdown.AddOptions(resOptions);
+        resolutionDropdown.value = currentResIndex;
+
+        // Detecta se jogo está em tela cheia e ajeita o Toggle.
+        fullscreenToogle.isOn = Screen.fullScreen;
+
+        // Detecta o volume principal do jogo seta ele e ajeita o Slider.
+
+        if (PlayerPrefs.HasKey("masterVolume"))
+            masterVolumeSlider.value = PlayerPrefs.GetFloat("masterVolume");
+        else {
+            PlayerPrefs.SetFloat("masterVolume", 1.0f);
+            masterVolumeSlider.value = PlayerPrefs.GetFloat("masterVolume");
+        }
+
+        AudioListener.volume = masterVolumeSlider.value;
+
+        // VOLUME DA MÚSICA (TODO)
+
+    }
 
     public void ToggleMenu (string id) {
 
@@ -43,6 +84,23 @@ public class MainMenu : MonoBehaviour {
 
         Application.Quit();
         Debug.Log("Application.Quit()");
+
+    }
+
+    public void ChangeScreenResolution() {
+
+        int index = resolutionDropdown.value;
+        bool isFullscreen = fullscreenToogle.isOn;
+
+        Screen.SetResolution(avaliableResolutions[index].width, avaliableResolutions[index].height, isFullscreen, avaliableResolutions[index].refreshRate);
+
+    }
+
+    public void ToggleMasterVolume() {
+
+        PlayerPrefs.SetFloat("masterVolume", masterVolumeSlider.value);
+        PlayerPrefs.Save();
+        AudioListener.volume = masterVolumeSlider.value;
 
     }
 }
