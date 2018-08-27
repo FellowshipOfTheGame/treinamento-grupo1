b86f45bc6c8b649a31be2ef6eb023ff51982969c
Unknown <luis.eduardo.rozante@gmail.com>
2018-08-27 16:26:38 -0300

    New effects.
    - Added paralax effects to background.
    - Added fire effect to player ability.

---
+++ Treinamento-2018-Grupo1/Assets/Scripts/Enviroment/Paralax.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+// Script que faz o efeito de paralax no fundo.
+[AddComponentMenu("Scripts/Enviroment/Paralax")]
+public class Paralax : MonoBehaviour {
+
+    [System.Serializable]
+    public class ParalaxLayer {
+
+        public GameObject layerObject;
+        public float x_multiplier = 1;
+        public float y_multiplier = 1;
+
+    }
+
+    public ParalaxLayer[] layers;
+
+    void Update () {
+        // "Move" cada camada em uma velocidade baseada na camera principal.
+        for (int i = 0; i < layers.Length; i++) {
+
+            Vector2 layer_pos = layers[i].layerObject.transform.position;
+            layer_pos.x = Camera.main.transform.position.x * layers[i].x_multiplier;
+            layer_pos.y = Camera.main.transform.position.y * layers[i].y_multiplier;
+
+            layers[i].layerObject.transform.position = layer_pos;
+
+        }
+
+	}
+}
