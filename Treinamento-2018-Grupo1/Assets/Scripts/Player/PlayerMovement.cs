c76a5b9cb6b83d69d7b70209a8ff7c3ac6c195ef
Unknown <luis.eduardo.rozante@gmail.com>
2018-08-27 15:07:17 -0300

    Fixed movimentation bugs.
    -Changed some parameters on player movimentation in an attempt to solve some bugs. Testing is still required.

--- Treinamento-2018-Grupo1/Assets/Scripts/Player/PlayerMovement.cs
+++ Treinamento-2018-Grupo1/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 
 // Realiza a movimentação do Player.
@@ -16,7 +14,7 @@ public class PlayerMovement : MonoBehaviour {
     private CapsuleCollider2D _collider; // Collider do target.
 
     public float velocity = 3.0f; // Velocidade do player.
-    public float airAcelerationMultiplier = 0.05f; // Modifica a aceleração do player no ar.
+    public float airAcelerationMultiplier = 0.025f; // Modifica a aceleração do player no ar.
     public float jumpHeight = 2.0f; // Altura maxima do pulo
 
     public LayerMask rayMask; // Mascára usada para filtrar raycasts.
@@ -45,18 +43,49 @@ public class PlayerMovement : MonoBehaviour {
             hNewVel = Mathf.Clamp(_rigidbody.velocity.x + Mathf.CeilToInt(target.input.horizontalAxis * velocity) * airAcelerationMultiplier, -velocity, velocity);
 
         // Movimento vertical.
-        vNewVel = _rigidbody.velocity.y;            
+            vNewVel = _rigidbody.velocity.y;
 
         // Verifica que esta tocando no chão.
-        grounded = Physics2D.CapsuleCast( transform.position + new Vector3(_collider.offset.x, _collider.offset.y, 0), new Vector2(_collider.size.x * 0.95f, _collider.size.y), CapsuleDirection2D.Vertical, 0, -Vector2.up, 0.075f, rayMask);
+        grounded = Physics2D.CapsuleCast( transform.position + new Vector3(_collider.offset.x, _collider.offset.y, 0), new Vector2(_collider.size.x, _collider.size.y), CapsuleDirection2D.Vertical, 0, -Vector2.up, 0.1f, rayMask);
+
+        /*
+        // Checa por rampas.
+        if (!grounded) {
+            RaycastHit2D hit;
+            float[] dotProduct = new float[2];
+
+            hit = Physics2D.Raycast(transform.position + new Vector3(_collider.offset.x, _collider.offset.y, 0) + new Vector3(_collider.size.x / 2, -_collider.size.y / 2 + 0.5f, 0), -Vector2.up, 0.55f, rayMask);
+            dotProduct[0] = Vector3.Dot(hit.normal, Vector2.up);
+
+            
+#if UNITY_EDITOR
+            if (hit.collider != null) {
+                if (0.64 < dotProduct[0] && dotProduct[0] < 0.76)
+                    Debug.DrawLine(transform.position + new Vector3(_collider.offset.x, _collider.offset.y, 0) + new Vector3(_collider.size.x / 2, -_collider.size.y / 2 + 0.5f, 0), transform.position + new Vector3(_collider.offset.x, -0.55f + -_collider.offset.y, 0) + new Vector3(_collider.size.x / 2, -_collider.size.y / 2 + 0.5f, 0), Color.red);
+                else
+                    Debug.DrawLine(transform.position + new Vector3(_collider.offset.x, _collider.offset.y, 0) + new Vector3(_collider.size.x / 2, -_collider.size.y / 2 + 0.5f, 0), transform.position + new Vector3(_collider.offset.x, -0.55f + -_collider.offset.y, 0) + new Vector3(_collider.size.x / 2, -_collider.size.y / 2 + 0.5f, 0), Color.yellow);
+            } else
+                Debug.DrawLine(transform.position + new Vector3(_collider.offset.x, _collider.offset.y, 0) + new Vector3(_collider.size.x / 2, -_collider.size.y / 2 + 0.5f, 0), transform.position + new Vector3(_collider.offset.x, -0.55f + -_collider.offset.y, 0) + new Vector3(_collider.size.x / 2, -_collider.size.y / 2 + 0.5f, 0), Color.green);
+#endif
+            
+
+        hit = Physics2D.Raycast(transform.position + new Vector3(_collider.offset.x, _collider.offset.y, 0) + new Vector3(-_collider.size.x / 2, -_collider.size.y / 2 - 0.5f, 0), -Vector2.up, 0.51f, rayMask);
+            dotProduct[1] = Vector3.Dot(hit.normal, Vector2.up);
+
+
+            if (0.64 < dotProduct[0] && dotProduct[0] < 0.76 || 0.64 < dotProduct[0] && dotProduct[0] < 0.76)
+                grounded = true;
+        }
+        */
 
         // DEBUG
-        #if UNITY_EDITOR
-        if (grounded)
+#if UNITY_EDITOR
+        if (grounded) {
             Debug.DrawLine(transform.position + new Vector3(_collider.offset.x, _collider.offset.y, 0) - new Vector3(0, _collider.size.y / 2, 0), transform.position + new Vector3(_collider.offset.x, _collider.offset.y, 0) - new Vector3(0, _collider.size.y / 2, 0) - new Vector3(0, _collider.size.y * 0.075f, 0), Color.red);
+        }
         else
             Debug.DrawLine(transform.position + new Vector3(_collider.offset.x, _collider.offset.y, 0) - new Vector3(0, _collider.size.y / 2, 0), transform.position + new Vector3(_collider.offset.x, _collider.offset.y, 0) - new Vector3(0, _collider.size.y / 2, 0) - new Vector3(0, _collider.size.y * 0.075f, 0), Color.green);
-        #endif
+#endif
 
         // Verifica se o player foi esmagado.
         if (grounded) {
@@ -66,27 +95,27 @@ public class PlayerMovement : MonoBehaviour {
                 gameObject.GetComponent<Player>().Death();
 
             // DEBUG
-            #if UNITY_EDITOR
+#if UNITY_EDITOR
             if (crushed)
                 Debug.DrawLine(transform.position + new Vector3(_collider.offset.x, _collider.offset.y, 0) + new Vector3(0, _collider.size.y / 2, 0), transform.position + new Vector3(_collider.offset.x, _collider.offset.y, 0) + new Vector3(0, _collider.size.y / 2, 0) + new Vector3(0, _collider.size.y * 0.05f, 0), Color.red);
             else
                 Debug.DrawLine(transform.position + new Vector3(_collider.offset.x, _collider.offset.y, 0) + new Vector3(0, _collider.size.y / 2, 0), transform.position + new Vector3(_collider.offset.x, _collider.offset.y, 0) + new Vector3(0, _collider.size.y / 2, 0) + new Vector3(0, _collider.size.y * 0.05f, 0), Color.green);
-            #endif
+#endif
         }
 
         // Verifica se o Player está tentando andar contra uma parede, usado para evitar que ele possa "grudar em paredes" durante um pulo.
-        bool blocked = Physics2D.CapsuleCast(transform.position + new Vector3(_collider.offset.x, _collider.offset.y, 0), new Vector2(_collider.size.x, _collider.size.y / 2), CapsuleDirection2D.Vertical, 0, Player.player.facing * Vector2.right, 0.05f, rayMask);
+        bool blocked = Physics2D.CapsuleCast(transform.position + new Vector3(_collider.offset.x, _collider.offset.y, 0), new Vector2(_collider.size.x, _collider.size.y * 0.95f), CapsuleDirection2D.Vertical, 0, Player.player.facing * Vector2.right, 0.05f, rayMask);
 
         if (blocked)
             hNewVel = 0;
 
         // DEBUG
-        #if UNITY_EDITOR
+#if UNITY_EDITOR
         if (blocked)
             Debug.DrawLine(transform.position + new Vector3(_collider.offset.x, _collider.offset.y, 0), transform.position + Player.player.facing * (new Vector3(0.05f, 0, 0) + new Vector3(_collider.size.x / 2, 0, 0)), Color.red);
         else
             Debug.DrawLine(transform.position + new Vector3(_collider.offset.x, _collider.offset.y, 0), transform.position + Player.player.facing * (new Vector3(0.05f, 0, 0) + new Vector3(_collider.size.x / 2, 0, 0)), Color.green);
-        #endif
+#endif
 
         // Evita a soma de addForces no pulo.
         if (target.input.jumpButton && grounded)
