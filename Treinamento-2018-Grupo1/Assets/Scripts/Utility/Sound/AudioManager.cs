517c43bf64f230fafac5d2de7c81a33f7b69cbb6
Unknown <luis.eduardo.rozante@gmail.com>
2018-09-11 22:25:39 -0300

    The battle continues!
    - Fixed some tile map bugs and music slider not working in pause menu!

--- Treinamento-2018-Grupo1/Assets/Scripts/Utility/Sound/AudioManager.cs
+++ Treinamento-2018-Grupo1/Assets/Scripts/Utility/Sound/AudioManager.cs
@@ -21,34 +21,49 @@ public class AudioManager : MonoBehaviour {
 		DontDestroyOnLoad(gameObject);
 
 		foreach(Sound s in sfxs){
-			if(s.source == null)s.source = gameObject.AddComponent<AudioSource>();
+
+			if(s.source == null)
+                s.source = gameObject.AddComponent<AudioSource>();
+
 			s.source.clip = s.clip;
 			s.source.loop = s.loop;
 			s.source.volume =  s.vol;
+
 		}
 
 		foreach(Sound s in musics){
-			if(s.source == null)s.source = gameObject.AddComponent<AudioSource>();
+
+			if(s.source == null)
+                s.source = gameObject.AddComponent<AudioSource>();
+
 			s.source.clip = s.clip;
 			s.source.loop = s.loop;
 			s.source.volume =  s.vol * volumeMusic;
+
 		}
 	}
 
 	public void mudarVolumeMusic(){
+
 		foreach(Sound s in musics){
 			s.source.volume =  s.vol * volumeMusic;
 		}
+
 	}
-
-
-
+    
 	public void play(string name, bool sfx){
-		Sound s = null;
-		if(sfx) s =Array.Find(sfxs, sound =>sound.name == name);
-		else s =Array.Find(musics, sound =>sound.name == name);
 
-		if(s !=null)s.source.Play();
-			else Debug.Log(name + " nao encontrado");
+		Sound s = null;
+
+		if(sfx)
+            s = Array.Find(sfxs, sound =>sound.name == name);
+		else
+            s = Array.Find(musics, sound =>sound.name == name);
+
+        if (s != null) {
+            s.source.Play();
+        } else
+            Debug.Log(name + " nao encontrado");
+
 	}
 }
