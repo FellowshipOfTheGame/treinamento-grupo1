f808b548e03c5a104b447695579091b15ad83956
Unknown <luis.eduardo.rozante@gmail.com>
2018-09-11 22:03:20 -0300

    Possible the final commit
    - If nothing crazy happens or some last minute change is needed this may be the final commit.

---
+++ Treinamento-2018-Grupo1/Assets/Scripts/Utility/PopUp.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+// Esse script faz com que um sprite aparece na tela quando o player está no trigger.
+[AddComponentMenu("Scripts/Utility/Pop Up")]
+public class PopUp : MonoBehaviour {
+
+    public SpriteRenderer target;
+
+    // Verifica quando o player entra na área.
+    void OnTriggerEnter2D(Collider2D collision) {
+
+        if (collision.gameObject.tag == "Player")
+            target.enabled = true;
+
+    }
+
+    // Verifica quando o player sai da área.
+    void OnTriggerExit2D(Collider2D collision) {
+
+        if (collision.gameObject.tag == "Player")
+            target.enabled = false;
+
+    }
+}
